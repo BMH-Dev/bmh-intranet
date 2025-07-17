95a117f1fb450958ef3c3ea93b4fe903daae5990
edo2019 <eddyyosphert@gmail.com>
2025-07-17 12:37:33 +0300

    Dashboard appearance updates

--- bmhIntranetApp/views.py
+++ bmhIntranetApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def demo(request):
-    data = range(1, 100)
+    data = range(1, 10)
     return render(request, 'dashboard.html', {'numg': data})
